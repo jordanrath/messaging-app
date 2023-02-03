ae6121834d81550742f817850719f2bc10a2c094
jordanrath <rath.jordan@icloud.com>
2023-02-03 14:03:02 -0700

    create reset page

--- src/Reset.js
+++ src/Reset.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { auth, sendPasswordReset } from "./Firebase";
 
 const Reset = () => {
+    const [email, setEmail] = useState("");
+    const [user, loading, error] = useAuthState(auth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (loading) return;
+        if (user) navigate('/dashboard');
+    }, [user, loading]);
+
   return (
-    <div>Reset</div>
+    <div className='reset'>
+        <div className='reset__container'>
+            <input 
+                type='text'
+                className='reset__textBox'
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                placeholder='E-mail Address'
+            />
+            <button 
+                className='reset__btn'
+                onClick={sendPasswordReset}
+            >
+                Send password reset email
+            </button>
+            <div>
+                Don't have an account? <Link to='/register'>Register</Link> now.
+            </div>
+        </div>
+    </div>
   )
 }
 
