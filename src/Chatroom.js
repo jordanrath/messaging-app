e2f3714b6e3f843ba02ad2f5e429fc564b61c90a
jordanrath <rath.jordan@icloud.com>
2023-02-07 16:31:44 -0700

    create and hook up ChatRoom.js

--- src/Chatroom.js
+++ src/Chatroom.js
@@ -1,74 +1,44 @@
-import React, { useRef, useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+  query,
+  collection,
+  limit,
+  orderBy,
+  onSnapshot,
+} from 'firebase/firestore';
+import { db } from './Firebase';
+import Message from './Message';
 import SendMessage from './SendMessage';
-import { auth, collection, addDoc, db } from './Firebase';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { FieldValue, getDocs, query } from 'firebase/firestore';
 
-const Chatroom = () => {
-    const dummy = useRef();
-    //const messagesRef = db.collection('messages');
-    //const query = messagesRef.orderBy('createdAt').limit(25);
-  
-    //const [messages] = useCollectionData(query, { idField: 'id' });
-    //const [messages] = query(collection(db, "messages"));
-  
-    const [formValue, setFormValue] = useState('');
-  
-    // const q = query(collection(db, "users"), where("uid", "==", user.uid));
-    // const docs = await getDocs(q);
-    // try {
-    //   const res = await signInWithPopup(auth, googleProvider);
-    //   const user = res.user;
-    //   const q = query(collection(db, "users"), where("uid", "==", user.uid));
-    //   const docs = await getDocs(q);
-    //   if (docs.docs.length === 0) {
-    //       await addDoc(collection(db, "users"), {
-    //           uid: user.uid,
-    //           name: user.displayName,
-    //           authProvider: "google",
-    //           email: user.email,
-    //       });
+const ChatRoom = () => {
+  const [messages, setMessages] = useState([]);
 
-    const sendMessage = async (e) => {
-        const { uid, photoURL } = auth.currentUser;
-        try {
-            e.preventDefault();
-            const q = query(collection(db, "messages"));
-            const docs = await getDocs(q)
-            if (docs.docs.length === 0) {
-            await addDoc(collection(db, "messages"), {
-                text: formValue,
-                createdAt: FieldValue.serverTimestamp(),
-                uid,
-                photoURL,
-            });
-            setFormValue('');
-            dummy.current.scrollIntoView({ behavior: 'smooth' });
-          }
-        } catch (err) {
-            console.error(err);
-            alert("There has been an error with the message you just tried to send.")
-        }
-      }
-      
-  
-    return (<>
-      <main>
-  
-        {/* {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)} */}
-  
-        <span ref={dummy}></span>
-  
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt"),
+      limit(50)
+    );
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let messages = [];
+      QuerySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe;
+  })
+
+    return (
+      <main className='chat-room'>
+        <div className='message-wrapper'>
+          {messages?.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
+        </div>
+        <SendMessage />
       </main>
-  
-      <form onSubmit={sendMessage}>
-  
-        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-  
-        <button type="submit" disabled={!formValue}>🕊️</button>
-  
-      </form>
-    </>)
+    )
   }
 
-export default Chatroom
+export default ChatRoom;
