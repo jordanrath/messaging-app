f90e3dbfd7157dd7e96eb592824698de95ebda1d
jordanrath <rath.jordan@icloud.com>
2023-02-22 21:11:20 -0700

    changed browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ChatRoom from './Components/ChatRoom';
 import Dashboard from './Components/Dashboard';
 import Login from './Components/Login';
