7b234c887e63b82aec40b013653bebdb264ddbfe
jordanrath <rath.jordan@icloud.com>
2023-02-28 15:28:50 -0700

    icon imports

--- src/Components/FontAwesomeIcons.js
+++ src/Components/FontAwesomeIcons.js
@@ -1,9 +1,4 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFacebook, faInstagram, faTwitter, faLinkedIn } from '@fortawesome/free-brands-svg-icons';
 
 library.add(faFacebook, faInstagram, faTwitter, faLinkedIn)
-
-
-<FontAwesomeIcon icon="fa-brands fa-facebook-f" />
-<FontAwesomeIcon icon="fa-brands fa-instagram" />
-<FontAwesomeIcon icon="fa-brands fa-twitter" />
-<FontAwesomeIcon icon="fa-brands fa-linkedin-in" />
