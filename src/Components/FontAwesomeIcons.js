7724b5a1ac38839f93bd84a7540d03fd7a7aca31
jordanrath <rath.jordan@icloud.com>
2023-02-28 15:27:54 -0700

    created fontawesome component

---
+++ src/Components/FontAwesomeIcons.js
@@ -0,0 +1,9 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(faFacebook, faInstagram, faTwitter, faLinkedIn)
+
+
+<FontAwesomeIcon icon="fa-brands fa-facebook-f" />
+<FontAwesomeIcon icon="fa-brands fa-instagram" />
+<FontAwesomeIcon icon="fa-brands fa-twitter" />
+<FontAwesomeIcon icon="fa-brands fa-linkedin-in" />
