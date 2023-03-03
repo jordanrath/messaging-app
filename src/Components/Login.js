007f04842e1f158640efc71ce0aa320237938008
jordanrath <rath.jordan@icloud.com>
2023-03-02 17:58:14 -0700

    deploy ghpages

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { 
-    auth,
+import {
     logInWithEmailAndPassword, 
     signInWithGoogle 
 } from '../Firebase';
